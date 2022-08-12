bf0caba175b7ad495b48451d1f7d6237901880ed
devinberens <devinlberens3@gmail.com>
2022-08-12 11:54:17 -0500

    NewExpenseInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from "./components/NewExpense/NewExpense";
+import React from 'react';
 function App() {
   const expenses = [
     {
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
