cccc45d7809301462f3da58560193e5abe34d62a
devinberens <devinlberens3@gmail.com>
2022-09-14 09:38:54 -0500

    expenseFilter from iMac

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,16 @@
-import React, {useState} from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated');
-    console.log('clicked')
-  };
-
-    return (
-    <Card className='expense-item'>
+  return (
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-        <button onClick={clickHandler}>Update</button>
     </Card>
   );
 }
